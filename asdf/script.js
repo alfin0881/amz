b0c06e7753db1c0b85c11abbba63fcc47b0cc831
AlfinZakaria <alfinzakaria108@gmail.com>
2025-02-19 20:52:34 +0700

    Add files via upload

--- asdf/script.js
+++ asdf/script.js
@@ -1,28 +1,37 @@
 let tombolTidak = document.getElementById("tombolTidak");
+let tombolYa = document.getElementById("tombolYa");
 let listKalimat = ["Coba Lagi", 
 "Semoga harimu error terus", 
 "You made a joke and everyone burst into laughter", 
 "What is surprise!!",
 "KLIK YA!!!🗣️",
 "i was laughing so hard",
-"Mungkin kamu lebih jago ",
+"Ga capek?",
 "404 not found",
 ];
-let kalimat = document.getElementById("kalimat");
 
-function yaDitekan() {
-    kalimat.innerHTML = "❤️Makasih Ya \^o^/❤️, https://github.com/alfin0881/amz";
-}
+let kalimat = document.getElementById("kalimat");
+let gambar = document.getElementById("gambar");
 function tidakDitekan() {
     kalimatRandom = Math.floor(Math.random() * listKalimat.length);
     kalimat.innerHTML = listKalimat[Math.floor(Math.random() * listKalimat.length)];
     tombolTidak.style.position = "absolute";
-    let angkaRandom = Math.floor(Math.random() * 500);
-    document.documentElement.clientWidth;
-    let angkaRandom2 = Math.floor(Math.random() * 600);
-    let angkaRandom3 = Math.floor(Math.random() * 600);
-    document.documentElement.clientHeight;
-    tombolTidak.style.top = angkaRandom + "px";
-    tombolTidak.style.right = angkaRandom2 + "px";
-    tombolTidak.style.left = angkaRandom3 + "px";
+
+    let angkaRandom = Math.floor(
+        Math.random() * document.documentElement.clientWidth
+    );
+    
+    let angkaRandom2 = Math.floor(
+        Math.random() * document.documentElement.clientHeight
+    );
+
+    tombolTidak.style.left = angkaRandom + "px";
+    tombolTidak.style.top = angkaRandom2 + "px";
+
+}
+function yaDitekan() {
+    kalimat.innerHTML = "❤️Makasih Ya \^o^/❤️, https://github.com/alfin0881/amz";
+    gambar.src = "happy.gif";
+    tombolYa.style.visibility = "hidden";
+    tombolTidak.style.visibility = "hidden";
 }
